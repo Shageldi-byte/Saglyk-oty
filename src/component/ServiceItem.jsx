e9462ca806d3d91aa10c29c3378221899cf259c0
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-02-16 11:23:43 +0500

    services updated

--- src/component/ServiceItem.jsx
+++ src/component/ServiceItem.jsx
@@ -1,18 +1,26 @@
 import React, { useContext } from "react";
+import { Controls, Player } from "@lottiefiles/react-lottie-player";
 import { Grid, Stack, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import { AppContext } from "../App.js";
 import { Fonts, colors } from "../common/theme.mjs";
-import {useTranslation} from "react-i18next";
 
 const ServiceItem = (props) => {
     const { isMobile } = useContext(AppContext);
-    const {t}=useTranslation();
+    const { t } = useTranslation();
     return (
         <div key={props.key}>
             <Grid container spacing={0} alignItems={'center'} sx={{ bgcolor: colors.solid, borderRadius: '22px', cursor: 'pointer' }}>
                 <Grid item xs={12} sm={12} md={3}>
                     <Stack sx={{ width: '100%', height: 'auto', pt: 10, pb: 10, pl: 5, pr: 5, bgcolor: colors.transparent, borderRadius: '22px' }} alignItems={'center'} justifyContent={'center'}>
-                        <img src={props.item.icon} alt={'service'} style={{ width: isMobile ? '30%' : '50%' }} />
+                        <img src={props.item.icon} alt={'service'} style={{ width: isMobile ? '50%' : '70%' }} />
+                        {/* <Player
+                            autoplay
+                            loop={true}
+                            src={'/images/drug.json'}
+                            style={{ width: isMobile ? '100%' : '100%', height: isMobile ? '50px' : '100px' }}
+                        >
+                        </Player> */}
                     </Stack>
                 </Grid>
                 <Grid item xs={12} sm={12} md={9}>
